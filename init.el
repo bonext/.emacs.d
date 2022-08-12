ede4a255a4aaf98759817f04a3cb42395f8bbbee
Alexander Molchanov <bonext@mailbox.org>
2022-08-12 21:41:11 +0200

    Redo init
    - removed some stale helper code
    - added evil
    - removed company

--- init.el
+++ init.el
@@ -1,44 +1,21 @@
-;; colors
+; colors
 (load-theme 'tango-dark)
 
-;;
-;; packages
-;;
-
+; packages
 (require 'package)
 
 ;; MELPA
-(add-to-list 
+(add-to-list
  'package-archives
  '("melpa" . "http://melpa.org/packages/")
  t)
 (package-initialize)
 
-;; stuff from teh internet
-;; https://www.reddit.com/r/emacs/comments/4fqu0a/automatically_install_packages_on_startup/d2ba42o
-;; === CUSTOM CHECK FUNCTION ===
-(defun ensure-package-installed (&rest packages)
-  "Assure every package is installed, ask for installation if it’s not.
-   Return a list of installed packages or nil for every skipped package."
-  (mapcar
-   (lambda (package)
-     (unless (package-installed-p package)
-       (package-install package)))
-   packages)
-  )
+;; evil
+(require 'evil)
+(evil-mode 1)
 
-;; === List my packages ===
-;; simply add package names to the list
-(ensure-package-installed
-  'company
-  ;; etc
-)
-
-;; company
-(add-hook 'after-init-hook 'global-company-mode)
-
-
-;; save position
+; save position
 (if (version< emacs-version "25.0")
     (progn
       (require 'saveplace)
