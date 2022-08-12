4317de5754d679d8d3d1e8ae7077ab91f094fea6
Alexander Molchanov <bonext@mailbox.org>
2022-08-12 21:55:32 +0200

    install slime

--- init.el
+++ init.el
@@ -22,9 +22,24 @@
 (require 'evil)
 (evil-mode 1)
 
+;; slime
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+
 ; save position
 (if (version< emacs-version "25.0")
     (progn
       (require 'saveplace)
       (setq-default save-place t))
   (save-place-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(slime evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
