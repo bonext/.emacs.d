40bbb6d2f4ab306281406ca698e3ff9dfdf74aa6
Alexander Molchanov <bonext@mailbox.org>
2022-09-03 19:34:47 +0200

    added lua-mode

--- init.el
+++ init.el
@@ -86,8 +86,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(paredit geiser-guile company slime evil)))
-(custom-set-faces)
+ '(package-selected-packages '(lua-mode paredit geiser-guile company slime evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
