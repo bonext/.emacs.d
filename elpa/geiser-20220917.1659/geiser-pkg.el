51ad9bc3f08fe33cb2718dbbb8002f08b9ec1b7f
Alexander Molchanov <bonext@mailbox.org>
2022-10-03 22:32:18 +0200

    update packages

---
+++ elpa/geiser-20220917.1659/geiser-pkg.el
@@ -0,0 +1,14 @@
+(define-package "geiser" "20220917.1659" "GNU Emacs and Scheme talk to each other"
+  '((emacs "25.1")
+    (transient "0.3")
+    (project "0.8.1"))
+  :commit "486c39cfbdf1789fe493535b7e0ae72ff518e53e" :authors
+  '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))
+  :maintainer
+  '("Jose Antonio Ortega Ruiz" . "jao@gnu.org")
+  :keywords
+  '("languages" "scheme" "geiser")
+  :url "https://gitlab.com/emacs-geiser/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
