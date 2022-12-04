17b33aa99e275adf651e4c80e8cedbd84ab73bcb
Alexander Molchanov <bonext@mailbox.org>
2022-12-04 20:18:05 +0100

    upd packages

---
+++ elpa/geiser-guile-20221127.509/geiser-guile-pkg.el
@@ -0,0 +1,14 @@
+(define-package "geiser-guile" "20221127.509" "Guile and Geiser talk to each other"
+  '((emacs "25.1")
+    (transient "0.3")
+    (geiser "0.28.1"))
+  :commit "f57bfd5039ce158de95d2ef2933e64fb16def4f6" :authors
+  '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))
+  :maintainer
+  '("Jose Antonio Ortega Ruiz" . "jao@gnu.org")
+  :keywords
+  '("languages" "guile" "scheme" "geiser")
+  :url "https://gitlab.com/emacs-geiser/guile")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/slime-20221127.2251/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20221127.2251" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "ba29269074534bd66758bd0a67082378e2587abe" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
