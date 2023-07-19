9c27a9681072ef420bd05436825627e72bcd18d7
Alexander Molchanov <bonext@mailbox.org>
2023-07-19 10:28:49 +0200

    update packages, install base16-theme

---
+++ elpa/geiser-20230714.38/geiser-pkg.el
@@ -0,0 +1,15 @@
+(define-package "geiser" "20230714.38" "GNU Emacs and Scheme talk to each other"
+  '((emacs "25.1")
+    (project "0.8.1"))
+  :commit "dc25a8868f39c9f572b051e9a7ea2b68f65aa2b2" :authors
+  '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))
+  :maintainers
+  '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))
+  :maintainer
+  '("Jose Antonio Ortega Ruiz" . "jao@gnu.org")
+  :keywords
+  '("languages" "scheme" "geiser")
+  :url "https://gitlab.com/emacs-geiser/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
