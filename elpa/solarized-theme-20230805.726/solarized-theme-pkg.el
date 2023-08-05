927eced13d8e4be82ad256ba45e1e1eff70d7644
Alexander Molchanov <bonext@mailbox.org>
2023-08-05 11:11:45 +0200

    update packages

--- elpa/solarized-theme-20230805.726/solarized-theme-pkg.el
+++ elpa/solarized-theme-20230805.726/solarized-theme-pkg.el
@@ -1,6 +1,6 @@
-(define-package "solarized-theme" "20230507.1413" "The Solarized color theme"
+(define-package "solarized-theme" "20230805.726" "The Solarized color theme"
   '((emacs "24.1"))
-  :commit "0f0b1129cf51c85904f9e015da4fcca5f5f7a4ce" :authors
+  :commit "100542418b19859f9828114eeaed2be1071367bb" :authors
   '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
   :maintainers
   '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
