440e6846496fd1f368382c41a42ff91bdcb44404
Alexander Molchanov <bonext@mailbox.org>
2023-08-17 09:42:44 +0200

    add org-capture templates

--- custom.el
+++ custom.el
@@ -5,6 +5,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-capture-templates
+   '(("b" "book" entry
+      (file "~/Documents/Notes/finished-books.org")
+      (file "~/.emacs.d/org-capture-templates/book")
+      :kill-buffer t)
+     ("d" "mind dump" entry
+      (file "~/Documents/Notes/mind-dumps.org")
+      (file "~/.emacs.d/org-capture-templates/dump")
+      :prepend t :kill-buffer t)))
  '(org-startup-truncated nil)
  '(package-selected-packages
    '(vimrc-mode solarized-theme slime paredit org-journal markdown-mode lua-mode geiser-racket geiser-guile evil-org company base16-theme)))
