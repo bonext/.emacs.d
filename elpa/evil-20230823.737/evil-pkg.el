1e07de7f25b99bb245b47d165866f28552d5a7b0
Alexander Molchanov <bonext@mailbox.org>
2023-08-25 21:42:04 +0200

    upd packages

---
+++ elpa/evil-20230823.737/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20230823.737" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "a667d8857f8a64249b53a4c45805ed6d62da4e5d" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/transient-20230825.1632/transient-pkg.el
@@ -0,0 +1,15 @@
+(define-package "transient" "20230825.1632" "Transient commands"
+  '((emacs "25.1")
+    (compat "29.1.4.1"))
+  :commit "7afa77974117afe45d4476513bf1eec82ba7f3d3" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainers
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("extensions")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
