c7b5c5c4090759476c7b6c5c6b8462bc1fc65394
Alexander Molchanov <bonext@mailbox.org>
2023-10-04 15:53:13 +0200

    upd packages

---
+++ elpa/slime-20230929.1057/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20230929.1057" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "46714e8eef318413c3e0a7c0cc3068a9fa296e11" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
