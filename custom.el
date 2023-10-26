b68bc093736a0a6c908f40c49b59c70aaabd9f9e
Alexander Molchanov <bonext@mailbox.org>
2023-10-26 09:51:07 +0200

    update custom

--- custom.el
+++ custom.el
@@ -16,7 +16,7 @@
       :prepend t :kill-buffer t)))
  '(org-startup-truncated nil)
  '(package-selected-packages
-   '(vimrc-mode solarized-theme slime paredit org-journal markdown-mode lua-mode geiser-racket geiser-guile evil-org company base16-theme)))
+   '(nix-mode vimrc-mode solarized-theme slime paredit org-journal markdown-mode lua-mode geiser-racket geiser-guile evil-org company base16-theme)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
