7c53724d4ea3de82bc655b997d98f116f1fbd1dc
Alexander Molchanov <bonext@mailbox.org>
2023-11-25 21:21:31 +0100

    upd packages

---
+++ elpa/company-20231123.1842/company-pkg.el
@@ -0,0 +1,14 @@
+(define-package "company" "20231123.1842" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "9b21604d19696de2c79ee28931620839b3a908b4" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainers
+  '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainer
+  '("Dmitry Gutov" . "dmitry@gutov.dev")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/slime-20231112.2019/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20231112.2019" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "735258a26bb97e85d25f39e4bef83c1f80c12f5d" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
