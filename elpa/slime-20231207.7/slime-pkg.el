8f04719da57163b3386e90711f9144d33db9dd38
Alexander Molchanov <bonext@mailbox.org>
2023-12-18 21:28:00 +0100

    update packages

--- elpa/slime-20231207.7/slime-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "slime" "20231207.7" "Superior Lisp Interaction Mode for Emacs"
-  '((cl-lib "0.5")
-    (macrostep "0.9"))
-  :commit "799a05f5b8cf4f65c1e90b802d0666f164aa7eb3" :keywords
-  '("languages" "lisp" "slime")
-  :url "https://github.com/slime/slime")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/slime-20231215.1307/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20231215.1307" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "2ca97012da4ab744ca008b7308b3be3c433c7c6b" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
