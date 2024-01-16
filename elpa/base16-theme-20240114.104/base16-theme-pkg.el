ea6c4ffb9894c3afa3ef5a92386c54c664ef37a1
Alexander Molchanov <bonext@mailbox.org>
2024-01-16 21:24:24 +0100

    upd packages

---
+++ elpa/base16-theme-20240114.104/base16-theme-pkg.el
@@ -0,0 +1,9 @@
+(define-package "base16-theme" "20240114.104" "Collection of themes built on combinations of 16 base colors" 'nil :commit "99ad4abecaf6060762f59f97f0859f1caab2194c" :authors
+  '(("Kaleb Elwert" . "belak@coded.io")
+    ("Neil Bhakta"))
+  :maintainer
+  '("Kaleb Elwert" . "belak@coded.io")
+  :url "https://github.com/tinted-theming/base16-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/slime-20231218.2206/slime-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "slime" "20231218.2206" "Superior Lisp Interaction Mode for Emacs"
-  '((cl-lib "0.5")
-    (macrostep "0.9"))
-  :commit "1be7fd2da04d52eee29a50ea06c5f121943c3c9f" :keywords
-  '("languages" "lisp" "slime")
-  :url "https://github.com/slime/slime")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
