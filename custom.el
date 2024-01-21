28829e21dfab5726c81983d3d1e94e8f8451142b
Alexander Molchanov <bonext@mailbox.org>
2024-01-21 23:07:30 +0100

    install projectile

--- custom.el
+++ custom.el
@@ -19,13 +19,18 @@
       :prepend t :kill-buffer t)))
  '(org-startup-truncated nil)
  '(package-selected-packages
-   '(nord-theme dts-mode nix-mode vimrc-mode solarized-theme slime paredit org-journal markdown-mode lua-mode geiser-racket geiser-guile evil-org company base16-theme)))
+   '(projectile nord-theme dts-mode nix-mode vimrc-mode solarized-theme slime paredit org-journal markdown-mode lua-mode geiser-racket geiser-guile evil-org company base16-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
