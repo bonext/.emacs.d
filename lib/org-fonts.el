02e36c26435762a269d0a6842daf153bb457b2a8
Alexander Molchanov <bonext@mailbox.org>
2024-09-06 23:44:43 +0200

    org: reorganise config, pull in some stuff from e-f-s

---
+++ lib/org-fonts.el
@@ -0,0 +1,67 @@
+(defun aa/org-setup-fonts ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords
+   'org-mode
+   '(("^ *\\([-]\\) "
+      (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  
+  ;; setup fonts
+  ;; via https://yannesposito.com/posts/0020-cool-looking-org-mode/index.html
+  ;; and https://zzamboni.org/post/beautifying-org-mode-in-emacs/
+  (let* (
+         ;; variable-width font setup
+         (variable-tuple
+          (cond
+           ((x-list-fonts "Source Sans Pro") '(:family "Source Sans Pro" :height 120))
+           ((x-list-fonts "IBM Plex Sans") '(:family "IBM Plex Sans" :height 120))
+           ((x-list-fonts "PT Serif") '(:family "PT Serif" :height 120))))
+         ;; fixed-width font setup
+         (fixed-tuple
+          (cond
+           ((x-list-fonts "Adobe Source Pro") '(:family "Adobe Source Pro"))
+           ((x-list-fonts "IBM Plex Mono") '(:family "IBM Plex Mono"))
+           ((x-list-fonts "PT Mono") '(:family "PT Mono"))))
+         ;; store default font color to reset headlines
+         (base-font-color (face-foreground 'default nil 'default))
+         ;; store some settings for headlines
+         (headline `(:inherit default :weight bold :foreground ,base-font-color)))
+    
+    (custom-theme-set-faces
+     ;; 'user is "current user custom settings"
+     'user
+
+     ;; variable and fixed pitch fonts
+     `(variable-pitch     ((t ,@variable-tuple)))
+     `(fixed-pitch        ((t ,@fixed-tuple)))
+
+     ;; headings
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.2))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.05))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple))))
+     `(org-level-8 ((t (,@headline ,@variable-tuple))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))
+
+     ;; things that should be fixed-pitch
+     '(org-ellipsis ((t (:inherit fixed-pitch :foreground "gray40" :underline nil))))
+     '(org-block            ((t (:inherit fixed-pitch))))
+     '(org-block-begin-line ((t (:inherit fixed-pitch))))
+     '(org-block-end-line   ((t (:inherit fixed-pitch))))
+     '(org-src              ((t (:inherit fixed-pitch))))
+     '(org-properties       ((t (:inherit fixed-pitch))))
+     '(org-code             ((t (:inherit (shadow fixed-pitch)))))
+     '(org-date             ((t (:inherit (shadow fixed-pitch)))))
+     '(org-document-info    ((t (:inherit (shadow fixed-pitch)))))
+     '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+     '(org-drawer           ((t (:inherit (shadow fixed-pitch)))))
+     '(org-indent           ((t (:inherit (org-hide fixed-pitch)))))
+     `(org-link             ((t (:inherit fixed-pitch :foreground ,base-font-color :underline t))))
+     '(org-meta-line        ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+     '(org-property-value   ((t (:inherit fixed-pitch))) t)
+     '(org-special-keyword  ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+     '(org-table            ((t (:inherit fixed-pitch))))
+     '(org-tag              ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+     '(org-verbatim         ((t (:inherit (shadow fixed-pitch))))))))
--- lib/org.el
+++ lib/org.el
@@ -1,131 +1,80 @@
-                                        ; ORG-MODE
-(setq org-directory "~/Documents/Notes")
+(defun aa/org-common-hooks ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
 
-;; populate org-agenda
-(load-library "find-lisp")
-(defun org-reload-my-agenda-files ()
-  (setq org-agenda-files
-        (append
-         (find-lisp-find-files org-directory "\.org$"))))
+(load (concat user-emacs-directory "lib/org-fonts.el"))
 
-;;; fonts and colors
-;;; via https://yannesposito.com/posts/0020-cool-looking-org-mode/index.html
-;;; and https://zzamboni.org/post/beautifying-org-mode-in-emacs/
-;;; make bold show bold and not *bold*
-(setq org-hide-emphasis-markers t)
-;;; variable pitch
-(add-hook 'org-mode-hook 'variable-pitch-mode)
-;;; set specific fonts for stuff
-;;; use (font-family-list) and C-j in scratch to list fonts
-(defun org-reset-my-fonts ()
-  (if (display-graphic-p)
-      (let* ((variable-tuple
-              (cond
-               ((x-list-fonts "PT Serif") '(:family "PT Serif" :height 120))))
-             (fixed-tuple
-              (cond
-               ((x-list-fonts "Adobe Source Pro") '(:family "Adobe Source Pro"))
-               ((x-list-fonts "IBM Plex Mono") '(:family "IBM Plex Mono"))
-               ((x-list-fonts "PT Mono") '(:family "PT Mono"))))
-             (base-font-color (face-foreground 'default nil 'default))
-             (headline `(:inherit default :weight bold :foreground ,base-font-color)))
-        (custom-theme-set-faces
-         'user
-         `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.4))))
-         `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.3))))
-         `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.2))))
-         `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-         `(org-level-5 ((t (,@headline ,@variable-tuple))))
-         `(org-level-6 ((t (,@headline ,@variable-tuple))))
-         `(org-level-7 ((t (,@headline ,@variable-tuple))))
-         `(org-level-8 ((t (,@headline ,@variable-tuple))))
-         `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))
-         `(variable-pitch     ((t ,@variable-tuple)))
-         `(fixed-pitch        ((t ,@fixed-tuple)))
+(use-package org
+  :hook (org-mode . aa/org-common-hooks)
+  :config
+  (setq org-directory "~/Documents/Notes")
+  ;; TODO: make the following part of fonts
+  (setq org-ellipsis " ▾")
+  (setq org-hide-emphasis-markers t)
+  ;; enable scaling of inline images with attr_org width
+  (setq org-image-actual-width nil)
+  ;; show inline images by default
+  (setq org-startup-with-inline-images t)
+  ;; Make C-c a t reuse current window
+  (setq org-agenda-window-setup 'current-window)
+  ;; offer tags from all agenda files (not current buffer)
+  (setq org-complete-tags-always-offer-all-agenda-tags t)
+  ;; wrap text by default
+  ;; TODO: do we need this?
+  (setq org-startup-truncated nil)
+  ;; log time when todos are done
+  ;; cf. https://orgmode.org/guide/Progress-Logging.html
+  (setq org-log-into-drawer "LOGBOOK")
+  (setq org-log-done 'time)
+  (setq org-todo-keywords
+        '((sequence "TODO(!)" "DONE(!)")))
+  (setq org-refile-targets
+        '((nil :maxlevel . 9)))
+  ;; refile in a single go
+  (setq org-outline-path-complete-in-steps nil)
+  ;; show full paths for refiling
+  (setq org-refile-use-outline-path t)
+  ;; capture setup
+  (setq org-default-notes-file (concat org-directory "/captured.org"))
+  (setq org-my-capture-templates-dir (concat user-emacs-directory "org-capture-templates"))
+  (setq org-capture-templates
+        '(("b" "book" entry
+           (file "~/Documents/Notes/finished-books.org")
+           (file "~/.emacs.d/book")
+           :kill-buffer t)
+          ("d" "mind dump" entry
+           (file "~/Documents/Notes/mind-dumps.org")
+           (file "~/.emacs.d/dump")
+           :prepend t :kill-buffer t)))
+  (aa/org-setup-fonts)
+  (global-set-key (kbd "C-c a") 'org-agenda)
+  (global-set-key (kbd "C-c c") 'org-capture)
+  (global-set-key (kbd "C-c l") 'org-store-link))
 
-         '(org-ellipsis ((t (:inherit fixed-pitch :foreground "gray40" :underline nil))))
-         '(org-block            ((t (:inherit fixed-pitch))))
-         '(org-block-begin-line ((t (:inherit fixed-pitch))))
-         '(org-block-end-line   ((t (:inherit fixed-pitch))))
-         '(org-src              ((t (:inherit fixed-pitch))))
-         '(org-properties       ((t (:inherit fixed-pitch))))
-         '(org-code             ((t (:inherit (shadow fixed-pitch)))))
-         '(org-date             ((t (:inherit (shadow fixed-pitch)))))
-         '(org-document-info    ((t (:inherit (shadow fixed-pitch)))))
-         '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
-         '(org-drawer           ((t (:inherit (shadow fixed-pitch)))))
-         '(org-indent           ((t (:inherit (org-hide fixed-pitch)))))
-         `(org-link             ((t (:inherit fixed-pitch :foreground ,base-font-color :underline t))))
-         '(org-meta-line        ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-         '(org-property-value   ((t (:inherit fixed-pitch))) t)
-         '(org-special-keyword  ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-         '(org-table            ((t (:inherit fixed-pitch))))
-         '(org-tag              ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
-         '(org-verbatim         ((t (:inherit (shadow fixed-pitch)))))))))
-
-;;; enable scaling of inline images with attr_org width
-(setq org-image-actual-width nil)
-
-;;; show inline images by default
-(setq org-startup-with-inline-images t)
-
-;;; capture setup
-(setq org-default-notes-file (concat org-directory "/captured.org"))
-
-;; Make C-c a t reuse current window
-(setq org-agenda-window-setup 'current-window)
-
-;; offer tags from all agenda files (not current buffer)
-(setq org-complete-tags-always-offer-all-agenda-tags t)
-
-;; wrap text by default
-(setq org-startup-truncated nil)
-
-;; log time when todos are done
-;; cf. https://orgmode.org/guide/Progress-Logging.html
-(setq org-log-into-drawer "LOGBOOK")
-(setq org-log-done 'time)
-(setq org-todo-keywords
-      '((sequence "TODO(!)" "DONE(!)")))
-
-(setq org-refile-targets
-      '((nil :maxlevel . 9)))
-(setq org-outline-path-complete-in-steps nil)         ; Refile in a single go
-(setq org-refile-use-outline-path t)                  ; Show full paths for refiling
-
-(setq org-my-capture-templates-dir (concat user-emacs-directory "org-capture-templates"))
-(setq org-capture-templates
-      '(("b" "book" entry
-         (file "~/Documents/Notes/finished-books.org")
-         (file "~/.emacs.d/book")
-         :kill-buffer t)
-        ("d" "mind dump" entry
-         (file "~/Documents/Notes/mind-dumps.org")
-         (file "~/.emacs.d/dump")
-         :prepend t :kill-buffer t)))
-
-;;; borrowed from suggested shortcut keys
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c l") 'org-store-link)
-
-(global-set-key (kbd "C-c r") 'org-reload-my-agenda-files)
-
-(org-reset-my-fonts)
-(org-reload-my-agenda-files)
-
-;; org-bullets
 (use-package org-bullets
-  :init
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun aa/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+       visual-fill-column-center-text t)
+ (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . aa/org-mode-visual-fill))
 
 ;; org-journal
 (use-package org-journal
+  :after org
   :init
   (setq org-journal-file-type 'daily)
   (setq org-journal-dir "~/Documents/journal")
   (setq org-journal-date-format "%Y-%m-%d, %A")
   (setq org-journal-file-format "%F.org") ; yyyy-mm-dd.org
-  (setq org-journal-encrypt-journal t)
-  :config
-  (global-set-key (kbd "C-c j") 'org-journal-new-entry))
+  (setq org-journal-encrypt-journal t))
+
+;; separate because the package load is deferred
+(global-set-key (kbd "C-c j") 'org-journal-new-entry)
