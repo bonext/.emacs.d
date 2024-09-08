7cea926d214151db29ecb9478a6bf959e6f0daee
Alexander Molchanov <bonext@mailbox.org>
2024-09-08 13:44:17 +0200

    osx-support: refactor colors

--- lib/colors.el
+++ lib/colors.el
@@ -17,12 +17,18 @@
 
 ;; use https://git.sr.ht/~grtcdr/darkman.el
 ;; to integrate with https://darkman.whynothugo.nl/
+(cond
+ ;; osx-specific setup goes here
+ (t (progn
+      (setq aa/light-theme 'solarized-selenized-white)
+      (setq aa/dark-theme 'gruvbox-dark-hard))))
+
 (if (file-exists-p "/usr/bin/darkman")
     (use-package darkman
       :config
-      (setq darkman-themes '(:light solarized-selenized-white :dark solarized-selenized-black))
+      (setq darkman-themes `(:light ,aa/light-theme :dark ,aa/dark-theme))
       (darkman-mode))
   ;; otherwise fallback to simpler solutions
   (cond
-   ((> (decoded-time-hour (decode-time)) 21) (load-theme 'solarized-selenized-black t))
-   (t (load-theme 'solarized-selenized-white t))))
+   ((> (decoded-time-hour (decode-time)) 20) (load-theme aa/dark-theme t))
+   (t (load-theme aa/light-theme t))))
