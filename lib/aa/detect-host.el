d6268ea3dd539a0e5370c887f8f76d279953516a
Alexander Molchanov <bonext@mailbox.org>
2025-05-16 09:20:16 +0200

    (core): expose host predicates

--- lib/aa/detect-host.el
+++ lib/aa/detect-host.el
@@ -1,8 +1,14 @@
-(setq aa/host
-      (cond
-       ((and (eq system-type 'gnu/linux)
-             (string-match "-[Mm]icrosoft" operating-system-release))
-        'wsl)
-       ((eq system-type 'darwin) 'work)
-       (t 'home)))
+;; -*- lexical-binding: t; -*-
+(let ((aa/host (cond
+                ((and (eq system-type 'gnu/linux)
+                      (string-match "-[Mm]icrosoft" operating-system-release))
+                 'wsl)
+                ((eq system-type 'darwin) 'work)
+                (t 'home))))
+  (defun aa/home-p ()
+    (eq aa/host 'home))
+  (defun aa/work-p ()
+    (eq aa/host 'work))
+  (defun aa/wsl-p ()
+    (eq aa/host 'wsl)))
 (provide 'aa/detect-host)
